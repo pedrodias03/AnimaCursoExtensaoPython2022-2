923fd1cdc9ce4eda86add32a688b0a0937bad9a2
pedrodias031 <117789443+pedrodias03@users.noreply.github.com>
2022-11-09 16:18:06 +0000

    aprendi a criar variavei e exibi-las na tela

--- main.py
+++ main.py
@@ -2,6 +2,15 @@
 #
 # print() = comando de saida
 print("Olá, mundo!")
+
+#Quando quiser guardar uma string
+nome = "Pedro Dias"
+#Quando quiser guardar um numero inteiro
+idade = 19
+
+#Exibir o meu nome
+print(nome)
+
 '''
 Comentario em bloco
 '''
